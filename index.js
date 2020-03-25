e6ff17634fdabe3d39af278f2156067f4862a82b
christy l tropila <ctropilalakebeverage@gmail.com>
2020-03-25 00:11:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function sayHiToGrandma(string){
  }
 
  if(string.toUpperCase() === string){
-   return "YES INDEED"
+   return "YES INDEED!"
  }
 
  if(string === "I love you, Grandma."){
-   return "I love you, too"
+   return "I love you, too."
  }
 
 }
